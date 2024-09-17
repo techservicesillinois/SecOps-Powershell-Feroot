54340876f60b812f8179dc9bfa30fe3c17add985
Tamara Buch <buch1@illinois.edu>
2024-09-17 10:24:07 -0500

    add vendordetails

--- src/UofIFeroot/UofIFeroot.psd1
+++ src/UofIFeroot/UofIFeroot.psd1
@@ -88,7 +88,8 @@ FunctionsToExport = @(
     'Set-FerootCrawlStatus',
     'Get-FerootDashboardData',
     'Get-FerootDataAssets',
-    'Get-FerootDataAssetDetails'
+    'Get-FerootDataAssetDetails',
+    'Get-FerootVendorDetails'
     )
 
 # Cmdlets to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no cmdlets to export.
---
+++ src/UofIFeroot/functions/public/Get-FerootVendorDetails.ps1
@@ -0,0 +1,55 @@
+﻿<#
+.Synopsis
+    Returns the details for a vendor's product's activity within a specified time range.
+.DESCRIPTION
+    Returns the details for a vendor's product's activity within a specified time range.
+.PARAMETER VendorID
+    The ID of a vendor's product
+.PARAMETER ProjectUUIDs
+    An array of Project UUIDs
+    Use Get-FerootProjects to get Project UUIDs
+.PARAMETER StartDate
+    Timestamp of the start of the date range
+.PARAMETER EndDate
+    Timestamp of the end of the date range
+.EXAMPLE
+    Get-FerootVendorDetails -StartDate (Get-Date).AddDays(-30) -EndDate (Get-Date) -VendorID "twitter"
+.EXAMPLE
+    Get-FerootVendorDetails -VendorID "yahoo" -ProjectUUIDs @('00000000-0000-0000-0000-000000000000') -StartDate (Get-Date).AddDays(-30) -EndDate (Get-Date)
+#>
+function Get-FerootVendorDetails{
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$VendorID,
+        [string[]]$ProjectUUIDs,
+        [Parameter(Mandatory=$true)]
+        [datetime]$StartDate,
+        [Parameter(Mandatory=$true)]
+        [datetime]$EndDate
+    )
+
+    process{
+
+        #Convert to UNIX Time
+        $Start = ([DateTimeOffset]$StartDate).ToUnixTimeSeconds()*1000
+        $End = ([DateTimeOffset]$EndDate).ToUnixTimeSeconds()*1000
+
+        # Complete URI with query parameters
+        $RelativeUri = "platform/vendors/details?vendorId=$($VendorID)&startDate=$($Start)&endDate=$($End)"
+
+        if($ProjectUUIDs){
+            $ProjectUUIDParam = $ProjectUUIDs | ForEach-Object { "projectUuids[]=$_" }
+            $ProjectUUIDParam = $ProjectUUIDParam -join "&"
+            $RelativeUri += "&$($ProjectUUIDParam)"
+        }
+
+        $RestSplat = @{
+            Method = 'GET'
+            RelativeURI = $RelativeUri
+        }
+
+        $Response = Invoke-FerootRestCall @RestSplat
+        $Response
+    }
+}
